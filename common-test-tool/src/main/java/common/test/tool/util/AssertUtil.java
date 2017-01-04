500298cccd1a7a3e9f1bc3b68115e1cea5ccbb4e
root <root@swaggerhub-registry-service-1>
2017-01-04 15:36:54 +0000

    Removed files managed by codegen

--- common-test-tool/src/main/java/common/test/tool/util/AssertUtil.java
+++
@@ -1,8 +0,0 @@
-package common.test.tool.util;
-
-public class AssertUtil {
-
-    public static <T> boolean isLambda(T e) {
-        return e.getClass().getSimpleName().contains("$$Lambda");
-    }
-}
